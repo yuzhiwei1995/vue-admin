b0c20c88ab8ca8cf05c3a71bbdda949e511cb439
yzw <2896019492@qq.com>
2020-10-28 22:23:54 +0800

    init

---
+++ src/views/table/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="app-container">
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      element-loading-text="Loading"
+      border
+      fit
+      highlight-current-row
+    >
+      <el-table-column align="center" label="ID" width="95">
+        <template slot-scope="scope">
+          {{ scope.$index }}
+        </template>
+      </el-table-column>
+      <el-table-column label="IP" width="150" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.ip }}
+        </template>
+      </el-table-column>
+      <el-table-column label="Port" width="110" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.port }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="Cert" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.fingerprint }}
+        </template>
+      </el-table-column>
+      <el-table-column class-name="status-col" label="Status" width="110" align="center">
+        <template slot-scope="scope">
+          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" prop="created_at" label="添加时间" width="200">
+        <template slot-scope="scope">
+          <i class="el-icon-time" />
+          <span>{{ scope.row.addTime }}</span>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+import { getList } from '@/api/table'
+
+export default {
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'gray',
+        deleted: 'danger'
+      }
+      let s = 'published';
+      if(status === 0){
+        s = 'draft'
+      } else if(status === -1){
+        s = 'deleted'
+      }
+      return statusMap[s]
+    }
+  },
+  data() {
+    return {
+      list: null,
+      listLoading: true
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.listLoading = true
+      getList().then(response => {
+        this.list = response.data.items
+        this.listLoading = false
+      })
+    }
+  }
+}
+</script>
